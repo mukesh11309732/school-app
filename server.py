90773d2974ec2208380a5aa16aa9e3cfe3deef13
prachipainuly <prachipainuly@gmail.com>
2026-02-21 16:33:02 +0100

    feat: add POST /extract endpoint to server

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import os
+import json
 import http.server
 import socketserver
-
 from http import HTTPStatus
+from app.extract_student_data import main
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -11,6 +12,28 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         msg = 'Hello! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
+
+    def do_POST(self):
+        if self.path == '/extract':
+            content_length = int(self.headers.get('Content-Length', 0))
+            body = self.rfile.read(content_length)
+            try:
+                args = json.loads(body)
+            except json.JSONDecodeError:
+                self.send_response(HTTPStatus.BAD_REQUEST)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps({"error": "Invalid JSON"}).encode())
+                return
+
+            result = main(args)
+            self.send_response(result.get("statusCode", 200))
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(result.get("body")).encode())
+        else:
+            self.send_response(HTTPStatus.NOT_FOUND)
+            self.end_headers()
 
 
 port = int(os.getenv('PORT', 80))
